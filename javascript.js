135c56725096bcdaff4998fa641e014617c8fc21
jakefernandesjf <46454692+jakefernandesjf@users.noreply.github.com>
2025-08-23 14:09:24 -0700

    Complete Book and myLibrary js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
 /* javascript.js */
+
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
